248bc4607beaaced553f9a9ae1318c7c6e4de5d0
NightRain <48847328+Iamdreaming@users.noreply.github.com>
2020-03-30 20:26:33 +0800

    栈

---
+++ stack.py
@@ -0,0 +1,30 @@
+'''
+@coding: utf-8
+@Description: 栈
+@version: 1.0
+@Author: 陈锐填
+@Date: 2020-03-29 10:47:46
+@LastEditors: 陈锐填
+@LastEditTime: 2020-03-30 18:58:29
+@FilePath: \结对项目\stack.py
+'''
+
+
+class Stack():
+    """栈"""
+
+    def __init__(self):
+        # 创建一个空的列表
+        self.items = []
+
+    def is_empty(self):
+        # 判空
+        return self.items == []
+
+    def push(self, item):
+        # 入栈
+        self.items.append(item)
+
+    def pop(self):
+        # 出栈
+        return self.items.pop()
