5bbf3d51480b5e275f9a5e727600891cfe578186
NightRain <48847328+Iamdreaming@users.noreply.github.com>
2020-03-30 20:27:52 +0800

    主函数

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 '''
-@Descripttion: 
+@Coding: utf-8
+@Description: 
 @version: 
 @Author: 陈锐填
 @Date: 2020-03-27 19:07:54
 @LastEditors: 陈锐填
-@LastEditTime: 2020-03-27 19:07:57
+@LastEditTime: 2020-03-29 11:48:21
+@FilePath: \结对项目\main.py
 '''
+
+
+def main():
+    pass
