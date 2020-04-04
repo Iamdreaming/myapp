a7cf0be63d3f69094de51b07b830c766eed09288
NightRain <48847328+Iamdreaming@users.noreply.github.com>
2020-04-04 20:13:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,24 @@
 @Author: 陈锐填
 @Date: 2020-03-27 19:07:54
 @LastEditors: 陈锐填
-@LastEditTime: 2020-03-29 11:48:21
+@LastEditTime: 2020-04-04 19:26:14
 @FilePath: \结对项目\main.py
 '''
+"""
+@Coding: utf-8
+@Description:
+@version:
+@Author: 陈锐填
+@Date: 2020-03-27 19:07:54
+@LastEditors: 陈锐填
+@LastEditTime: 2020-03-29 11:48:21
+@FilePath: \结对项目\main.py
+"""
 
-
+from szys import SZYS
 def main():
-    pass
+    a = SZYS(10000,10)
+    a.store()
+
+if __name__ == '__main__':
+    main()
