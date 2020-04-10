b2230afe5c16ba5a1243d1154bd64c2490f43c9f
NightRain <48847328+Iamdreaming@users.noreply.github.com>
2020-04-10 18:05:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 @Author: 陈锐填
 @Date: 2020-03-27 19:07:54
 @LastEditors: 陈锐填
-@LastEditTime: 2020-04-05 15:25:37
+@LastEditTime: 2020-04-06 08:36:21
 @FilePath: \结对项目\main.py
 '''
 """
@@ -23,9 +23,7 @@ from szys import SZYS
 def main():
     a = SZYS(10000,10)
     a.store()
+    print('yes')
 
 if __name__ == '__main__':
-    t1 = time()
     main()
-    t2 = time()
-    print(t2-t1)
